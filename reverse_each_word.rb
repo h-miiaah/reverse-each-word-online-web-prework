ef6112ebe1e9f13aca98bb33ec97894a69b7caf3
Haim D <haim11293@aol.com>
2019-06-29 00:28:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  new_array = []
+  original_array.collect do |string|
+    new_array << string.reverse
+  end
+  new_array.join(" ")
+end
